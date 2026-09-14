331b4e3955ed36ce0a936c856e8ee407b743e8f5
albertinaripanu01 <aripanu@spartaglobal.com>
2026-09-14 11:49:40 +0100

    Initial project commit

---
+++ src/test/java/com/example/spoon/SpoonprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoonprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
