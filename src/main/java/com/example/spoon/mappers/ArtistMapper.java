647535c6b56032de7671a81d653348d22b929317
albertinaripanu01 <aripanu@spartaglobal.com>
2026-09-15 10:18:33 +0100

    Update files

--- src/main/java/com/example/spoon/mappers/ArtistMapper.java
+++ src/main/java/com/example/spoon/mappers/ArtistMapper.java
@@ -1,4 +1,13 @@
 package com.example.spoon.mappers;
 
+import com.example.spoon.dtos.AlbumDTO;
+import com.example.spoon.dtos.ArtistDTO;
+import com.example.spoon.entities.Album;
+import com.example.spoon.entities.Artist;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ArtistMapper {
+    ArtistDTO toDTO(Artist artist);
+    Artist toEntity(ArtistDTO artistDTO);
 }
--- src/main/java/com/example/spoon/mappers/SongMapper.java
+++ src/main/java/com/example/spoon/mappers/SongMapper.java
@@ -1,4 +1,13 @@
 package com.example.spoon.mappers;
 
+
+import com.example.spoon.dtos.SongDTO;
+
+import com.example.spoon.entities.Song;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface SongMapper {
+    SongDTO toDTO(Song song);
+    Song toEntity(SongDTO songDTO);
 }
