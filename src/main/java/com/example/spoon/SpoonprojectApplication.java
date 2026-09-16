4610090f72eb1cb061952911e2bbfe6fed71e51d
kf304 <kacperfelczak11@gmail.com>
2026-09-16 12:07:56 +0100

    added workflow

--- src/main/java/com/example/spoon/SpoonprojectApplication.java
+++ src/main/java/com/example/spoon/SpoonprojectApplication.java
@@ -2,8 +2,10 @@ package com.example.spoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.spoon.mappers")
 public class SpoonprojectApplication {
 
     public static void main(String[] args) {
