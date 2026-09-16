14baa8fdb0c8249bf41e02ccd83fb25de5abe266
albertinaripanu01 <aripanu@spartaglobal.com>
2026-09-16 10:07:26 +0100

    create albumconstructor

--- src/main/java/com/example/spoon/controllers/AlbumController.java
+++ src/main/java/com/example/spoon/controllers/AlbumController.java
@@ -1,7 +1,58 @@
 package com.example.spoon.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spoon.dtos.AlbumDTO;
+import com.example.spoon.services.AlbumService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AlbumController {
+    private final AlbumService service;
+    public AlbumController(AlbumService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AlbumDTO>> getAllAlbums() {
+        return ResponseEntity.ok(service.getAllAlbums());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AlbumDTO> getAlbumById(@PathVariable Integer id) {
+        AlbumDTO album = service.getAlbumById(id);
+        if (album != null) {
+            return ResponseEntity.ok(album);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<AlbumDTO> addAlbum(@RequestBody AlbumDTO albumDTO) {
+        AlbumDTO savedAlbum = service.saveAlbum(albumDTO);
+        return ResponseEntity.status(201).body(savedAlbum);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlbumDTO> updateAlbum(@PathVariable Integer id, @RequestBody AlbumDTO albumDTO) {
+        albumDTO.setId(id);
+        try {
+            AlbumDTO updatedAlbum = service.updateAlbum(albumDTO);
+            return ResponseEntity.ok(updatedAlbum);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAlbum(@PathVariable Integer id) {
+        boolean deleted = service.deleteAlbum(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
