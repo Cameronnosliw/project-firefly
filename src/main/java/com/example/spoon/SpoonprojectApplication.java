2b0b4dfff3f168d7196c77ac3ef4e2a02f0a9d0b
albertinaripanu01 <aripanu@spartaglobal.com>
2026-09-16 10:26:16 +0100

    make the swagger work

--- src/main/java/com/example/spoon/SpoonprojectApplication.java
+++ src/main/java/com/example/spoon/SpoonprojectApplication.java
@@ -2,10 +2,8 @@ package com.example.spoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.example.spoon.mappers")
 public class SpoonprojectApplication {
 
     public static void main(String[] args) {
