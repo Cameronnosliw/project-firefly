b7414b3edaf4c59095df77893579bf43d133e8f9
Cameron Wilson <ctwilson159@gmail.com>
2026-09-16 09:49:26 +0100

    Fixed Mapping issues

--- src/main/java/com/example/spoon/controllers/ArtistController.java
+++ src/main/java/com/example/spoon/controllers/ArtistController.java
@@ -1,7 +1,30 @@
 package com.example.spoon.controllers;
 
+import com.example.spoon.dtos.ArtistDTO;
+import com.example.spoon.services.ArtistService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/artists")
 public class ArtistController {
+
+    private final ArtistService artistService;
+
+    public ArtistController(ArtistService artistService) {this.artistService = artistService;}
+
+    @Operation(summary = "Get all artists")
+    @GetMapping
+    public ResponseEntity<List<ArtistDTO>> getAllArtists() {
+        List<ArtistDTO> artists = artistService.getAllArtists();
+        return ResponseEntity.ok(artists);
+    }
+
 }
