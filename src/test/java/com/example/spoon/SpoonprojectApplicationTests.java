fd6f4b6813b3dc21138af5387b9e325428bf4a47
Thambi <piravienfive@gmail.com>
2026-09-21 14:21:57 +0100

    add h2 data source to fix contextload()

--- src/test/java/com/example/spoon/SpoonprojectApplicationTests.java
+++ src/test/java/com/example/spoon/SpoonprojectApplicationTests.java
@@ -3,12 +3,14 @@ package com.example.spoon;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpoonprojectApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
